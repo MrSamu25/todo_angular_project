fa4448868b498e2e9aafe2b941efe695933681d3
Steven Muriel <samu250797@gmail.com>
2021-02-01 20:00:04 -0500

    deleting default code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('todoProject');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('todoProject app is running!');
-  });
 });
